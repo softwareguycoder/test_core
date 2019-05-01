4a508d5458e06dc6ccf8457259ea213e105e348a
softwareguycoder <softwareguycoder@gmail.com>
2019-05-01 08:27:08 -0400

    Added reference to api_core library

--- test_core/include/stdafx.h
+++ test_core/include/stdafx.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <../../api_core/api_core/include/api_core.h>
 #include <../../common_core/common_core/include/common_core.h>
 
 #endif //__TEST_CORE_STDAFX_H__
