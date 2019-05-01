71daafdc8ac887e6942ceddba98c8a59a17cbd95
softwareguycoder <softwareguycoder@gmail.com>
2019-05-01 08:12:06 -0400

    Adding assert_api.c/.h

---
+++ test_core/include/assert_api.h
@@ -0,0 +1,10 @@
+// assert_api.h - Definitions of the Assert* functions that make up a testing
+// api
+//
+
+#ifndef __ASSERT_API_H__
+#define __ASSERT_API_H__
+
+// TODO: Add symbol definitions and function prototypes here
+
+#endif //__ASSERT_API_H__
